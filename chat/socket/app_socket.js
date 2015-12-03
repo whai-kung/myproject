a32a68d021eb9e7f3c31d1a72ed1414d509b1370
singlewhai <n.ratanasuwan@gmail.com>
2015-12-04 01:30:12 +0400

    text chat room and socket

--- chat/socket/app_socket.js
+++ chat/socket/app_socket.js
@@ -4,8 +4,8 @@ var fs = require('fs')
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html'));
-}).listen(8080, function() {
-    console.log('Listening at: http://localhost:8080');
+}).listen(3000, function() {
+    console.log('Listening at: http://localhost:3000');
 });
 socketio.listen(server).on('connection', function (socket) {
     socket.on('message', function (msg) {
